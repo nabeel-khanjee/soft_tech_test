b94de5e58b88ebec06616b3d70bddaa993ae3306
Muhammad Nabeel <nabeel@gmail.com>
2024-01-15 17:05:52 +0500

    fixed

--- lib/src/pages/doctor_detail/components/expansion_panel_faq.dart
+++ lib/src/pages/doctor_detail/components/expansion_panel_faq.dart
@@ -29,69 +29,70 @@ class _ExpansionPanelFaqsState extends State<ExpansionPanelFaqs> {
           expandIconColor: Theme.of(context).scaffoldBackgroundColor,
           dividerColor: ColorConstants.black,
           children: [
-            ExpansionPanel(
-              hasIcon: false,
-              backgroundColor: !getThemeStateIsLight()
-                  ? darken(getThemeColor(context), 0.35)
-                  : ColorConstants.white,
-              isExpanded: isExpanded ?? false,
-              canTapOnHeader: true,
-              headerBuilder: (context, isExpanded) => GestureDetector(
-                onTap: () {
-                  setState(() {
-                    this.isExpanded = !isExpanded;
-                  });
-                },
-                child: Container(
-                  margin: EdgeInsets.zero,
-                  padding: const EdgeInsets.all(10),
-                  width: double.infinity,
-                  height: 60,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(15),
-                    color: !getThemeStateIsLight()
-                        ? darken(getThemeColor(context), 0.35)
-                        : ColorConstants.white,
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        widget.faq.value.title,
-                        style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                            fontFamily: FontConstantc.gilroyMedium,
-                            color: Theme.of(context).scaffoldBackgroundColor),
-                      ),
-                      !isExpanded
-                          ? Icon(
-                              Icons.add,
-                              color: getThemeStateIsLight()
-                                  ? darken(getThemeColor(context), 0.35)
-                                  : ColorConstants.white,
-                            )
-                          : Container(
-                              margin: const EdgeInsets.all(4),
-                              width: 15,
-                              height: 2,
-                              color: getThemeStateIsLight()
-                                  ? darken(getThemeColor(context), 0.35)
-                                  : ColorConstants.white,
-                            )
-                    ],
-                  ),
-                ),
-              ),
-              body: Container(
-                padding: const EdgeInsets.all(10),
-                child: Text(
-                  widget.faq.value.desc,
-                  style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                      color: getThemeStateIsLight()
-                          ? darken(getThemeColor(context), 0.3)
-                          : ColorConstants.white),
-                ),
-              ),
-            ),
+            // ExpansionPanel(
+            //   hasIcon: false,
+            //   backgroundColor: !getThemeStateIsLight()
+            //       ? darken(getThemeColor(context), 0.35)
+            //       : ColorConstants.white,
+            //   isExpanded: isExpanded ?? false,
+            //   canTapOnHeader: true,
+            //   headerBuilder: (context, isExpanded) => GestureDetector(
+            //     onTap: () {
+            //       setState(() {
+            //         this.isExpanded = !isExpanded;
+            //       });
+            //     },
+            //     child: Container(
+            //       margin: EdgeInsets.zero,
+            //       padding: const EdgeInsets.all(10),
+            //       width: double.infinity,
+            //       height: 60,
+            //       decoration: BoxDecoration(
+            //         borderRadius: BorderRadius.circular(15),
+            //         color: !getThemeStateIsLight()
+            //             ? darken(getThemeColor(context), 0.35)
+            //             : ColorConstants.white,
+            //       ),
+            //       child: Row(
+            //         mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            //         children: [
+            //           Text(
+            //             widget.faq.value.title,
+            //             style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+            //                 fontFamily: FontConstantc.gilroyMedium,
+            //                 color: Theme.of(context).scaffoldBackgroundColor),
+            //           ),
+            //           !isExpanded
+            //               ? Icon(
+            //                   Icons.add,
+            //                   color: getThemeStateIsLight()
+            //                       ? darken(getThemeColor(context), 0.35)
+            //                       : ColorConstants.white,
+            //                 )
+            //               : Container(
+            //                   margin: const EdgeInsets.all(4),
+            //                   width: 15,
+            //                   height: 2,
+            //                   color: getThemeStateIsLight()
+            //                       ? darken(getThemeColor(context), 0.35)
+            //                       : ColorConstants.white,
+            //                 )
+            //         ],
+            //       ),
+            //     ),
+            //   ),
+            //   body: Container(
+            //     padding: const EdgeInsets.all(10),
+            //     child: Text(
+            //       widget.faq.value.desc,
+            //       style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+            //           color: getThemeStateIsLight()
+            //               ? darken(getThemeColor(context), 0.3)
+            //               : ColorConstants.white),
+            //     ),
+            //   ),
+            // ),
+    
           ],
         ),
       ),
